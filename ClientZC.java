904eddb3a0f404b88efb423eb8b625acbe6489a7
PatcharaTee <deedeebaker8@gmail.com>
2021-03-15 13:48:40 +0700

    upload file

---
+++ ClientZC.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ZeroCopyDownloadFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Patchara
+ */
+public class ClientZC {
+
+    public static void main(String[] args) {
+        //load server config
+        ClientConfig ccf = new ClientConfig();
+        //Set up - Client can set place to store files, server and port at class ClientConfig
+        int port = ccf.getPort();
+        String server = ccf.getServer();
+        File clientFolder = new File(ccf.getClientFolder());
+        Scanner scan = new Scanner(System.in);
+        OUTER:
+        try {
+            //Try to connect to server and Wait for user interaction
+            SocketChannel clientSock = SocketChannel.open();
+            clientSock.connect(new InetSocketAddress(server, port));
+            if (clientSock.isConnected()) {
+                System.out.println("Connected to server.");
+            }
+            ByteBuffer byteBuff;
+            while (true) {
+                System.out.print("Type Files to get file list.\n"
+                        + "     Download to download file.\n"
+                        + "     Exit to disconnect.\n"
+                        + "Input = ");
+                //Send command to server
+                String sendComm = scan.nextLine();
+                byteBuff = ByteBuffer.wrap(sendComm.getBytes("UTF-8"));
+                clientSock.write(byteBuff);
+                switch (sendComm) {
+                    case "Files":
+                        System.out.println("--- FILE ---");
+                        //Recive the number of file from server
+                        byteBuff = ByteBuffer.allocate(4);
+                        clientSock.read(byteBuff);
+                        byteBuff.flip();
+                        int numberOfFile = byteBuff.getInt();
+                        //Recvie all the file name from server
+                        byteBuff = ByteBuffer.allocate(1024);
+                        clientSock.read(byteBuff);
+                        byteBuff.flip();
+                        String[] fileName = new String(byteBuff.array(), "UTF-8").trim().split(" ");
+                        for (int i = 0; i < numberOfFile; i++) {
+                            System.out.println((i + 1) + ": " + fileName[i]);
+                        }
+                        break;
+                    case "Download":
+                        System.out.print("File name = ");
+                        //Send selected file name to server
+                        String selectedFile = scan.nextLine();
+                        System.out.print("Name to save = ");
+                        String nameToSave = scan.nextLine();
+                        byteBuff = ByteBuffer.wrap(selectedFile.getBytes("UTF-8"));
+                        clientSock.write(byteBuff);
+                        //Recive file size from server
+                        byteBuff = ByteBuffer.allocate(4);
+                        clientSock.read(byteBuff);
+                        byteBuff.flip();
+                        int fileSize = byteBuff.getInt();
+                        if (fileSize > 0) {
+                            System.out.println("Downloading....");
+                            try (FileChannel output = new FileOutputStream(clientFolder.getPath() + "/" + nameToSave, true).getChannel()) {
+                                DecimalFormat df = new DecimalFormat("#.##");
+                                byteBuff = ByteBuffer.allocate(1500);
+                                int progress = 0;
+                                int count = 0;
+                                while (progress != fileSize) {
+                                    progress += (int) clientSock.read(byteBuff);
+                                    byteBuff.flip();
+                                    output.write(byteBuff);
+                                    byteBuff.clear();
+                                    count++;
+                                    if (count % 50 == 0) {
+                                        System.out.println("Progress = " + df.format((progress / (double) fileSize) * 100) + "%");
+                                    }
+                                }
+                            }
+                            System.out.println("Finish.");
+                        } else {
+                            System.out.println("File not found.");
+                        }
+                        break;
+                    case "Exit":
+                        System.out.println("Disconnected from server.");
+                        clientSock.close();
+                        break OUTER;
+                    default:
+                        System.out.println("Wrong input.");
+                        break;
+                }
+                System.out.println("");
+            }
+        } catch (IOException ex) {
+            System.out.println("Can't connect to the Server.");
+            //System.out.println(ex);
+        }
+    }
+}
+
+class ClientConfig {
+
+    private final int port = 20;
+    private final String server = "127.0.0.1";
+    private final String clientFolder = "C:/Users/Admin/Desktop/APP/FTPTestPath/ClientFolder";
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public String getClientFolder() {
+        return clientFolder;
+    }
+}
---
+++ ServerZC.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ZeroCopyDownloadFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Patchara
+ */
+public class ServerZC {
+
+    public static void main(String[] args) {
+        //load server config
+        ServerConfig scf = new ServerConfig();
+        //Set up - Admin can set file path and port at class ServerConfig
+        int port = scf.getPort();
+        File serverFolder = new File(scf.getServerFolder());
+        //Time stamp
+        LocalDateTime timeStamp = LocalDateTime.now();
+        try {
+            //Open Connection
+            ServerSocketChannel serverSock = ServerSocketChannel.open();
+            serverSock.bind(new InetSocketAddress(port));
+            System.out.println(timeStamp + ": Server started.");
+            //Wait for connection
+            while (true) {
+                SocketChannel clientSock = serverSock.accept();
+                if (clientSock.isConnected()) {
+                    System.out.println(timeStamp + ": Client accept. " + clientSock.getLocalAddress());
+                }
+                //New client thread
+                ClientHandler handler = new ClientHandler(clientSock, serverFolder);
+                handler.start();
+            }
+        } catch (IOException ex) {
+            System.out.println(timeStamp + ": Cannot start the server.");
+            //System.out.println(ex);
+        }
+    }
+}
+
+class ServerConfig {
+
+    private final int port = 6666;
+    private final String serverFolder = "C:/Users/Admin/Desktop/APP/FTPTestPath/ServerFolder";
+
+    public String getServerFolder() {
+        return serverFolder;
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
+
+class ClientHandler extends Thread {
+
+    File serverFolder;
+    SocketChannel clientSock;
+
+    public ClientHandler(SocketChannel clientSock, File serverFolder) {
+        this.clientSock = clientSock;
+        this.serverFolder = serverFolder;
+    }
+
+    @Override
+    public void run() {
+        File[] files = serverFolder.listFiles();
+        ByteBuffer byteBuff;
+        OUTER:
+        while (true) {
+            try {
+                //Time stamp
+                LocalDateTime timeStamp = LocalDateTime.now();
+                //Recive command from client
+                byteBuff = ByteBuffer.allocate(32);
+                clientSock.read(byteBuff);
+                byteBuff.flip();
+                String reciveComm = new String(byteBuff.array(), "UTF-8").trim();
+                switch (reciveComm) {
+                    case "Files":
+                        System.out.println(timeStamp + ": Client " + clientSock.getLocalAddress() + " request downloadable file list.");
+                        //Send the number of file to client
+                        int numberOfFile = files.length;
+                        byteBuff = ByteBuffer.allocate(4);
+                        byteBuff.putInt(numberOfFile);
+                        byteBuff.flip();
+                        clientSock.write(byteBuff);
+                        //Send all the file name to client
+                        String fileName = "";
+                        for (int i = 0; i < numberOfFile; i++) {
+                            fileName += files[i].getName() + " ";
+                        }
+                        byteBuff = ByteBuffer.wrap(fileName.getBytes("UTF-8"));
+                        clientSock.write(byteBuff);
+                        break;
+                    case "Download":
+                        //Recive selected file name from client
+                        byteBuff = ByteBuffer.allocate(32);
+                        clientSock.read(byteBuff);
+                        byteBuff.flip();
+                        String selectedFile = new String(byteBuff.array(), "UTF-8").trim();
+                        //Check if there are files on the server or not
+                        int fileSize = 0;
+                        for (File file : files) {
+                            if (file.getName().equals(selectedFile)) {
+                                fileSize = (int) file.length();
+                                break;
+                            }
+                        }
+                        //Send file size to client
+                        byteBuff = ByteBuffer.allocate(4);
+                        byteBuff.putInt(fileSize);
+                        byteBuff.flip();
+                        clientSock.write(byteBuff);
+                        if (fileSize > 0) {
+                            System.out.println(timeStamp + ": Client " + clientSock.getLocalAddress() + " request file " + selectedFile + " / File size = " + fileSize + " byte");
+                            try (FileChannel input = new FileInputStream(serverFolder.getPath() + "/" + selectedFile).getChannel()) {
+                                byteBuff = ByteBuffer.allocate(1500);
+                                //int progress = 0;
+                                //int startIndex = 0;
+                                long start = System.currentTimeMillis();
+                                /*
+                                while (progress != fileSize) {
+                                    progress += input.transferTo(startIndex, 1500, clientSock);
+                                    startIndex += 1500;
+                                }
+                                 */
+                                while (input.read(byteBuff) != -1) {
+                                    byteBuff.flip();
+                                    clientSock.write(byteBuff);
+                                    byteBuff.clear();
+                                }
+                                long end = System.currentTimeMillis();
+                                System.out.println(timeStamp + ": Finish tranafer file to client " + clientSock.getLocalAddress() + ", transfer time = " + (end - start) + "ms");
+                            }
+                        } else {
+                            System.out.println(timeStamp + ": Client " + clientSock.getLocalAddress() + " request file " + selectedFile + " / File not found.");
+                        }
+                        break;
+                    case "Exit":
+                        System.out.println(timeStamp + ": Client " + clientSock.getLocalAddress() + " disconnected. " + clientSock.getLocalAddress());
+                        break OUTER;
+                    default:
+                        break;
+                }
+            } catch (IOException ex) {
+                //System.out.println(ex);
+            }
+        }
+    }
+}
